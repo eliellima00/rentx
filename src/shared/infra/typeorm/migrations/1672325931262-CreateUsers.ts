9a4c6a09cae69ace168811482dde9be29235c2e0
eliellima00 <eliel.lima@attosementes.com.br>
2023-02-17 16:14:51 -0400

    feat: add migration create rentals table

--- src/shared/infra/typeorm/migrations/1672325931262-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1672325931262-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1672325931262 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
