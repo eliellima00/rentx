fdc0712b8f2eb001588050a7525d33f3df3269c4
eliellima00 <eliel.lima@attosementes.com.br>
2023-03-17 08:48:14 -0400

    feat: find rental by user

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -7,12 +7,19 @@ import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 const rentalRoutes = Router();
 const createRentalController = new CreateRentalController();
 const devolutionRentalController = new DevolutionRentalController();
+const listRentalsByUserController = new DevolutionRentalController();
 
 rentalRoutes.post("/", ensureAuthenticated, createRentalController.handle);
 rentalRoutes.post(
   "/devolution/:id",
   ensureAuthenticated,
   devolutionRentalController.handle
+);
+
+rentalRoutes.get(
+  "/user",
+  ensureAuthenticated,
+  listRentalsByUserController.handle
 );
 
 export { rentalRoutes };
