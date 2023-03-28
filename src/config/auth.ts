b1570c2d6d2d6bbef4713753cc2a3c3818126ed6
eliellima00 <eliel.lima@attosementes.com.br>
2023-03-28 09:37:47 -0400

    feat: add refresh token

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "b2b64e63550246a76f6ac51e2724ae55",
+  expires_in_token: "15m",
+  secret_refresh_token: "a45768230c7ea60bbf335355f9de34c2",
+  expires_in_refresh_token: "30d",
+  expires_refresh_tokens_days: 30,
+};
