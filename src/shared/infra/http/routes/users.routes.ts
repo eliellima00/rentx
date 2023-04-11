5d8a7f6b92f47c96758590a24eaf90c7d9e56d8f
eliellima00 <eliel.lima@attosementes.com.br>
2023-04-11 16:33:54 -0400

    feat: add aws s3 provider

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -2,13 +2,13 @@ import { Router } from "express";
 import multer from "multer";
 
 import uploadConfig from "../../../../config/upload";
-import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateUserController } from "../../../../modules/accounts/useCases/createUser/CreateUserController";
 import { UpdateUserAvatarController } from "../../../../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const usersRoutes = Router();
 
-const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
+const uploadAvatar = multer(uploadConfig);
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
